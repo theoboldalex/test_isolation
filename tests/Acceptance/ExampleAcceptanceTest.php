2f60d8c0c390f4605105370bf1f26aecc85ed542
theoboldalex <theoboldalex@gmail.com>
2022-11-10 19:28:54 +0000

    Adds example classes

---
+++ tests/Acceptance/ExampleAcceptanceTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Acceptance;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleAcceptanceTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/ExampleUnitTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleUnitTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
